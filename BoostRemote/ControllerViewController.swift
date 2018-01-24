95ea0031f59aff811f4047ca03f3accefc5624e1
Shinichiro Oba <ooba@bricklife.com>
2018-01-24 02:23:25 +0000

    Enable Haptic Feedback

--- BoostRemote/ControllerViewController.swift
+++ BoostRemote/ControllerViewController.swift
@@ -29,6 +29,16 @@ class ControllerViewController: UIViewController {
         }
     }
     
+    private let feedbackGenerator: Any? = {
+        if #available(iOS 10.0, *) {
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            return generator
+        } else {
+            return nil
+        }
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -93,8 +103,18 @@ class ControllerViewController: UIViewController {
     }
     
     private func sendCommand(port: BoostBLEKit.Port, power: Int8) {
+        if power == 100 || power == -100 {
+            feedback()
+        }
+        
         if let command = motors[port]?.powerCommand(power: power) {
             ActionCenter.send(command: command)
+        }
+    }
+    
+    private func feedback() {
+        if #available(iOS 10.0, *), let generator = feedbackGenerator as? UISelectionFeedbackGenerator {
+            generator.selectionChanged()
         }
     }
     
