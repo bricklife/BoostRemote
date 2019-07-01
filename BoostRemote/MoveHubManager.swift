f032335674aea8a921d98ddd052049fda17fdc68
Shinichiro Oba <ooba@bricklife.com>
2019-07-01 16:10:01 +0900

    Update BoostBLEKit

--- BoostRemote/MoveHubManager.swift
+++ BoostRemote/MoveHubManager.swift
@@ -49,8 +49,12 @@ class MoveHubManager: NSObject {
         switch hubType {
         case .boost:
             self.connectedHub = Boost.MoveHub()
+        case .boostV1:
+            self.connectedHub = Boost.MoveHubV1()
         case .poweredUp:
             self.connectedHub = PoweredUp.SmartHub()
+        case .duploTrain:
+            self.connectedHub = Duplo.TrainBase()
         }
         
         self.peripheral = peripheral
